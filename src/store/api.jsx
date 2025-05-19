afe1465f73fb8536aa1bbd3f28beed925dc9c8e0
Kuba Klin <poczta@kubaklin.pl>
2025-05-19 16:03:44 +0200

    feat: redux store (list & single)

---
+++ src/store/api.jsx
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const api = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.tvmaze.com/' }),
+  tagTypes: ['Shows'],
+  endpoints: (builder) => ({
+    getShows: builder.query({
+      query: () => '/shows',
+      providesTags: ['Shows'],
+    }),
+    getSingleShow: builder.query({
+      query: (id) => `shows/${id}`,
+      providesTags: ['Shows'],
+    }),
+  }),
+});
+
+export const { useGetShowsQuery, useGetSingleShowQuery } = api;
